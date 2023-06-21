6ec894a3fadee0091adbd5f1400e2e70c0fb4d2a
callaghan210-coder <skiprop336@gmail.com>
2023-06-21 08:44:18 +0300

    my first commit

---
+++ Myscript.py
@@ -0,0 +1,6 @@
+# import pandas as pd
+
+# df = pd.read_csv('C:/Users/Sharon/Desktop/melb_data.csv')
+
+# print(df.head())
+https://www.kaggle.com/code/dansbecker/basic-data-exploration?scriptVersionId=126670606&cellId=4
